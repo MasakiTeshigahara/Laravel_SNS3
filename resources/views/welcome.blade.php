27c9196cb8e62e55cdebe7ce1ee90ea3d68b6d67
MasakiTeshigahara <maskai.teshi@gamail.com>
2020-08-16 14:24:58 +0900

    トップボタン変更３

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                <img src="data:image/png;base64,{{ $images[7]->image }}" alt="ツイート一覧" ,
+                <img src="data:image/png;base64,{{ $images[5]->image }}" alt="ツイート一覧" ,
                 class="center-block" ,width="200" height="50" style="margin-right: 220px;"></a>
                 </div>
 
